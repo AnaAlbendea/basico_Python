838b54e71ba047391c3609e8e0717fe76b5a2d61
Mañanas <anasalvasor@gmail.com>
2024-03-08 13:39:26 +0100

    Ejerciciobasicopython

---
+++ Diccionario.py
@@ -0,0 +1,10 @@
+
+
+# Crear los diccionarios
+diccionario1 = {'a': 1, 'b': 2, 'c': 3}
+diccionario2 = {'d': 4, 'e': 5, 'f': 6}
+
+# Unir los diccionarios
+diccionario_final = {**diccionario1, **diccionario2}
+
+print(diccionario_final)
